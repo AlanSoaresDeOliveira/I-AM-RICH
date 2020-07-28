a10823da18eee69e725ff5f9579477f3259a59db
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-07-28 16:09:49 -0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Alan Soares on 28/07/20.
+//  Copyright © 2020 Alan Soares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
